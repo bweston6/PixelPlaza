8dbefe52cb99b259d7253b84d8b7ef230076f3ed
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-08-04 20:04:22 +0100

    Adding social links.

--- components/common/footer.jsx
+++ components/common/footer.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 function Footer() {
 	return (
 		<footer>
@@ -7,11 +9,11 @@ function Footer() {
 				</div>
 				<nav className="secondary-navigation" aria-label="secondary">
 					<ul className="even-columns small-gap" role="list">
-						<li><a href="#"><i className="fa-brands fa-instagram" /></a></li>
-						<li><a href="#"><i className="fa-brands fa-tiktok" /></a></li>
-						<li><a href="#"><i className="fa-brands fa-twitter" /></a></li>
-						<li><a href="#">Privacy Policy</a></li>
-						<li><a href="#">Terms of Service</a></li>
+						<li><a href="https://instagram.com"><i className="fa-brands fa-instagram" /></a></li>
+						<li><a href="https://tiktok.com"><i className="fa-brands fa-tiktok" /></a></li>
+						<li><a href="https://twitter.com"><i className="fa-brands fa-twitter" /></a></li>
+						<li><Link href="/privacy-policy"><a>Privacy Policy</a></Link></li>
+						<li><Link href="/tos"><a>Terms of Service</a></Link></li>
 					</ul>
 				</nav>
 			</div>
